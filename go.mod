a42424f2d43f2183857dc3cb098e39ab896c4b40
Leo Goulart <leonardogoulartt1@gmail.com>
2025-09-24 17:46:31 -0300

    added postgres connection

--- go.mod
+++ go.mod
@@ -43,6 +43,7 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
---
+++ internal/sentinel/connections/postgres.go
@@ -0,0 +1,107 @@
+package connections
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+type PostgresConnectionConfig struct {
+	Host        string
+	Port        int
+	User        string
+	Password    string
+	Database    string
+	SSLMode     string
+	SSLRootCert string
+	SSLVerify   bool
+}
+
+type PostgresConnection struct {
+	DB *sql.DB
+}
+
+func NewPostgresConnectionConfig(config map[string]any) (*PostgresConnectionConfig, error) {
+	for _, field := range []string{"host", "port", "user", "password", "database"} {
+		if _, ok := config[field]; !ok {
+			return nil, fmt.Errorf("missing required field: %s", field)
+		}
+	}
+
+	c := PostgresConnectionConfig{
+		Host:      config["host"].(string),
+		Port:      getIntValue(config["port"]),
+		User:      config["user"].(string),
+		Password:  config["password"].(string),
+		Database:  config["database"].(string),
+		SSLMode:   config["sslmode"].(string),
+		SSLVerify: config["sslverify"].(bool),
+	}
+
+	if sslMode, ok := config["sslmode"].(string); ok {
+		c.SSLMode = sslMode
+	}
+
+	if sslRootCert, ok := config["sslrootcert"].(string); ok {
+		c.SSLRootCert = sslRootCert
+	}
+
+	if sslVerify, ok := config["sslverify"].(bool); ok {
+		c.SSLVerify = sslVerify
+	}
+
+	if c.Host == "" {
+		return nil, fmt.Errorf("missing required field: host")
+	}
+	if c.Port == 0 {
+		c.Port = 5432
+	}
+	if c.User == "" {
+		return nil, fmt.Errorf("missing required field: user")
+	}
+	if c.Password == "" {
+		return nil, fmt.Errorf("missing required field: password")
+	}
+	if c.Database == "" {
+		return nil, fmt.Errorf("missing required field: database")
+	}
+
+	return &c, nil
+}
+
+func (c *PostgresConnection) Close() error {
+	return c.DB.Close()
+}
+
+func NewPostgresConnection(config PostgresConnectionConfig) (*PostgresConnection, error) {
+	var db *sql.DB
+	var err error
+
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		config.Host,
+		config.Port,
+		config.User,
+		config.Password,
+		config.Database,
+		config.SSLMode,
+	)
+
+	if config.SSLMode != "disable" && !config.SSLVerify {
+		dsn += " sslrootcert= sslcert= sslkey="
+	}
+
+	db, err = sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %v", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("failed to ping database: %v", err)
+	}
+
+	return &PostgresConnection{
+		DB: db,
+	}, nil
+}
